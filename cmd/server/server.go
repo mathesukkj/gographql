093faf8fe25db67e3d5202c0d46ab5ba264b76e5
mathesukkj <kemuel.g7363@gmail.com>
2024-05-30 20:27:17 -0300

    feat: add sqlite conn

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+
 	"github.com/mathesukkj/gographql/graph"
+	"github.com/mathesukkj/gographql/internal/database"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open db: %v", err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(
+		graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+			CategoryDB: categoryDb,
+		}}),
+	)
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
