d436d8a8156fa03c652f1fbf82b4b9b116c57515
mathesukkj <kemuel.g7363@gmail.com>
2024-05-30 20:43:09 -0300

    feat: add category findall

--- internal/database/category.go
+++ internal/database/category.go
@@ -34,3 +34,20 @@ func (c *CategoryDb) Create(name string, description string) (Category, error) {
 
 	return Category{ID: id, Name: name, Description: description}, nil
 }
+
+func (c *CategoryDb) FindAll() ([]Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	categories := []Category{}
+	for rows.Next() {
+		var id, name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, Category{ID: id, Name: name, Description: description})
+	}
+	return categories, nil
+}
