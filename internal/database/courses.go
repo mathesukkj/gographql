26578d8607fb311966d3e7ff32b2fcadf4d36437
mathesukkj <kemuel.g7363@gmail.com>
2024-05-30 21:06:34 -0300

    feat: add categories + courses nested queries

--- internal/database/courses.go
+++ internal/database/courses.go
@@ -56,3 +56,26 @@ func (c *CourseDb) FindAll() ([]Course, error) {
 	}
 	return courses, nil
 }
+
+func (c *CourseDb) FindByCategoryId(categoryId string) ([]Course, error) {
+	rows, err := c.db.Query(
+		"SELECT id, name, description, category_id FROM courses WHERE category_id = $1",
+		categoryId,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	courses := []Course{}
+	for rows.Next() {
+		var id, name, description, categoryId string
+		if err := rows.Scan(&id, &name, &description, &categoryId); err != nil {
+			return nil, err
+		}
+		courses = append(
+			courses,
+			Course{ID: id, Name: name, Description: description, CategoryId: categoryId},
+		)
+	}
+	return courses, nil
+}
